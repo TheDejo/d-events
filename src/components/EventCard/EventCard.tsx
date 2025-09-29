d2136a7a53519c23bb66004912c0d07a4b3848aa
TheDejo <divineogbe@gmail.com>
2025-09-29 20:25:05 +0100

    logger

--- src/components/EventCard/EventCard.tsx
+++ src/components/EventCard/EventCard.tsx
@@ -76,6 +76,7 @@ export default function EventCard({
                className={styles.eventCardImage} 
                fill
                priority={true}
+               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
              />
               <div className={styles.imageFooter}>
                {hasTracks && (
